dc4f71d229a0232b76e76f2cc604f9b99419b1dc
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-16 16:49:29 +0100

    Test the kernel the way kernel_tuner wants.

--- AOFlagger.py
+++ AOFlagger.py
@@ -3,22 +3,36 @@ import kernel_tuner
 import numpy
 
 
+#
 # Test swap()
-def test_swap():
+#
+def swap_control(data, x, y):
+    temp = data[x]
+    data[x] = data[y]
+    data[y] = temp
+
+
+def test_swap(data_control, data_device):
+    numpy.isclose(data_control, data_device, atol=1.0e-06)
+
+
+def swap():
     with open("swap.cu") as file:
         kernel_source = file.read()
     input_size = 8192
-    array_device = numpy.random.randn(input_size).astype(numpy.float32)
-    array_host = array_device
-    arguments = [array_device, numpy.uint32(0), numpy.uint32(8191)]
+    data_device = numpy.random.randn(input_size).astype(numpy.float32)
+    data_control = data_device
+    swap_control(data_control, 0, 8191)
+    arguments_device = [data_device, numpy.uint32(0), numpy.uint32(8191)]
+    arguments_control = [data_control, None, None]
     parameters = {}
-    kernel_tuner.run_kernel("swap", kernel_source, input_size, arguments, parameters, quiet=True)
-    if abs(array_host[0] - array_device[8191]) > 1.0e-06:
-        print("Error: " + str(array_host[0]) + " != " + str(array_device[8191]))
-        return -1
+    kernel_tuner.tune_kernel("swap", kernel_source, input_size, arguments_device, parameters, answer=arguments_control,
+                            verify=test_swap, quiet=True)
 
 
+#
 # Run the tests
+#
 if __name__ == "__main__":
     print("Testing function: swap()")
-    test_swap()
+    swap()
