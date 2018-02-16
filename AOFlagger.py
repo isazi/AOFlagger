89c4e618249f12ae7c946526e542acc963ad213e
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-16 14:26:24 +0100

    Running the swap() function.

--- AOFlagger.py
+++ AOFlagger.py
@@ -6,3 +6,18 @@ FILENAME = "AOFlagger.cu"
 
 with open(FILENAME) as file:
     kernel_source = file.read()
+
+
+# Test swap()
+def test_swap():
+    input_size = 8192
+    array_device = numpy.random.randn(input_size).astype(numpy.float32)
+    # array_host = array_device
+    arguments = [array_device]
+    parameters = []
+    kernel_tuner.run_kernel("swap", kernel_source, input_size, arguments, parameters)
+
+
+# Run the tests
+if __name__ == "__main__":
+    test_swap()
