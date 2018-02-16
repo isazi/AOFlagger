15308cc452de1a4b83ccbe255cec9d9fa9be0c8b
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-16 16:35:12 +0100

    Testing the function.

--- AOFlagger.py
+++ AOFlagger.py
@@ -9,12 +9,16 @@ def test_swap():
         kernel_source = file.read()
     input_size = 8192
     array_device = numpy.random.randn(input_size).astype(numpy.float32)
-    # array_host = array_device
+    array_host = array_device
     arguments = [array_device, numpy.uint32(0), numpy.uint32(8191)]
     parameters = {}
     kernel_tuner.run_kernel("swap", kernel_source, input_size, arguments, parameters)
+    if abs(array_host[0] - array_device[8191]) > 1.0e-06:
+        print("Error: " + array_host[0] + " != " + array_device[8191])
+        return -1
 
 
 # Run the tests
 if __name__ == "__main__":
+    print("Testing function: swap()")
     test_swap()
