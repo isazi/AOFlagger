6cf9159d00cc36bba661bdf32e77191a2c65223c
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-16 16:57:18 +0100

    Use parametric size.

--- AOFlagger.py
+++ AOFlagger.py
@@ -21,8 +21,8 @@ def swap():
     input_size = 8192
     data_device = numpy.random.randn(input_size).astype(numpy.float32)
     data_control = data_device
-    swap_control(data_control, 0, 8191)
-    arguments_device = [data_device, numpy.uint32(0), numpy.uint32(8191)]
+    swap_control(data_control, 0, input_size - 1)
+    arguments_device = [data_device, numpy.uint32(0), numpy.uint32(input_size - 1)]
     arguments_control = [data_control, None, None]
     tuning_parameters = {"block_size_x" : {1024}}
     kernel_tuner.tune_kernel("swap", kernel_source, input_size, arguments_device, tuning_parameters,
