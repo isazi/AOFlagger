55d8de838661c8d3a5a3e14bca1bb729a30bb650
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-23 11:43:44 +0100

    Added unused dimensions.

--- AOFlagger.py
+++ AOFlagger.py
@@ -13,13 +13,16 @@ def tune_statistics():
     tuning_parameters = dict()
     tuning_parameters["type"] = "float"
     tuning_parameters["threads_per_block"] = [threads for threads in range(32, 1024 + 1, 32)]
+    tuning_parameters["threads_per_block_other_dim"] = [1]
     tuning_parameters["items_per_thread"] = [items for items in range(1, 256, 1)]
     tuning_parameters["items_per_block"] = [items for items in range(1, input_size + 1)]
     constraints = ["(" + str(input_size) + " % items_per_block) == 0",
                    "(items_per_block % (threads_per_block * items_per_thread)) == 0"]
+    block_size_names = ["threads_per_block", "threads_per_block_other_dim", "threads_per_block_other_dim"]
     try:
         results = kernel_tuner.tune_kernel("compute_statistics_1D", Statistics.Statistics.generate_cuda, input_size,
-                                           kernel_arguments, tuning_parameters, lang="CUDA", restrictions=constraints)
+                                           kernel_arguments, tuning_parameters, lang="CUDA", restrictions=constraints,
+                                           block_size_names=block_size_names)
     except Exception as error:
         print(error)
 
